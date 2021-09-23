5539765d29f8b0368738f7ac3e70a89123089fa7
BeautyVika <vikachk54@gmail.com>
2021-09-23 14:59:42 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict";
+
+//  const result = confirm("Are you here?");
+//  console.log(result);
+
+//  const answer = +prompt("Вам есть 18?", "18");
+//  console.log(answer + 5);
+
+//  const answers = [];
+ 
+//  answers[0] = prompt('Как ваше имя?', '');
+//  answers[1] = prompt('Как ваше фамилия?', '');
+//  answers[2] = prompt('Cколько вам лет?', '');
+
+// console.log(typeof(answers));
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`);
+
+console.log('arr' + " - object");
+console.log(4 + +"5");
+
+let incr = 10,
+     decr = 10;
+
+// ++incr;
+// --decr;
+
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2 + 2 * 2 !==  '6');
+
+const isChecked = false,
+       isClose = false;
+console.log(isChecked || !isClose); 
