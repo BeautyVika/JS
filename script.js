4483b3d73a8e16142bbf38706b30afa7d9f09dc2
BeautyVika <vikachk54@gmail.com>
2021-09-24 15:19:17 +0300

    add script.js

--- script.js
+++ script.js
@@ -1,40 +1,21 @@
 "use strict";
+const numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', '');
 
-//  const result = confirm("Are you here?");
-//  console.log(result);
+const personalMovieDB = {
+     count: numberOfFilms,
+     movies: {},
+     actors: {},
+     gernes: [],
+     privat: false
+};
 
-//  const answer = +prompt("Вам есть 18?", "18");
-//  console.log(answer + 5);
+const a = prompt('Один из последних просмотренных фильмов?', ''),
+      b = prompt('Насколько оцените его?', ''),
+      c = prompt('Один из последних просмотренных фильмов?', ''),
+      d = prompt('Насколько оцените его?', '');
+      
+      
+  personalMovieDB.movies[a] = b;
+  personalMovieDB.movies[c] = d;
 
-//  const answers = [];
- 
-//  answers[0] = prompt('Как ваше имя?', '');
-//  answers[1] = prompt('Как ваше фамилия?', '');
-//  answers[2] = prompt('Cколько вам лет?', '');
-
-// console.log(typeof(answers));
-
-// const user = "Ivan";
-
-// alert(`Привет, ${user}`);
-
-console.log('arr' + " - object");
-console.log(4 + "5");
-
-let incr = 10,
-     decr = 10;
-
-// ++incr;
-// --decr;
-
-
-console.log(++incr);
-console.log(--decr);
-
-console.log(5%2);
-
-console.log(2 + 2 * 2 !==  '6');
-
-const isChecked = false,
-       isClose = false;
-console.log(isChecked || !isClose); 
+  console.log(personalMovieDB);
